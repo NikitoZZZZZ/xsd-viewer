65a8ed015ea273a673f615788bc845597d5232e2
NikitoZZZZZ <manakovnikita95@ya.ru>
2017-07-23 15:09:36 +0300

    XSDParser added

---
+++ src/main/java/com/emc/xsdviewer/server/XSDParser/XSDNodeHandler.java
@@ -0,0 +1,32 @@
+package com.emc.xsdviewer.xsdparser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class XSDNodeHandler {
+	XSDNodeHandler() {
+	}
+	
+	public static String createAttrName(Node node) {
+		return (node.getAttributes().getNamedItem("name") != null)
+				? node.getAttributes().getNamedItem("name").toString()
+				: null;
+	}
+	
+	public static List<XSDNode> createNextNodeList(Node node) {
+		List<XSDNode> nextNodeList = new ArrayList<>();
+		if (node.hasChildNodes()) {
+			NodeList nodeList = node.getChildNodes();
+			for (int i = 0; i < nodeList.getLength(); ++i) {
+				if (nodeList.item(i).getNodeType() != Node.TEXT_NODE) {
+					nextNodeList.add(new XSDNode(nodeList.item(i)));
+				}
+			}
+		}
+		
+		return nextNodeList;
+	}
+}
