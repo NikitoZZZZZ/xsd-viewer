b75420b5366d8ce2ec6f5546cf348ef40aca4a99
Tolkachev Nikita <nictol76@gmail.com>
2017-07-27 02:24:49 +0300

    Error fixed

--- client/src/main/resources/static/js/main.controller.js
+++ client/src/main/resources/static/js/main.controller.js
@@ -1,4 +1,4 @@
-angular.module("WebApp").controller('ctrl', ['$scope', '$http', function($scope, $http) {
+var app=angular.module("WebApp").controller('ctrl', ['$scope', '$http', function($scope, $http) {
     $scope.postFile = function() {
         var xsd = angular.element("schema");
         $http.post("localhost:8765/upload", xsd);
