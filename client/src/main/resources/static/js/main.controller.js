40399f8fd0379c286da7c15d15219bc0dd214c6e
Tolkachev Nikita <nictol76@gmail.com>
2017-07-27 10:35:19 +0300

    Error fixed

--- client/src/main/resources/static/js/main.controller.js
+++ client/src/main/resources/static/js/main.controller.js
@@ -1,6 +1,53 @@
-var app=angular.module("WebApp").controller('ctrl', ['$scope', '$http', function($scope, $http) {
-    $scope.postFile = function() {
-        var xsd = angular.element("schema");
-        $http.post("localhost:8765/upload", xsd);
+var app = angular.module("WebApp", []).controller('ctrl', function ($scope, $http) {
+
+    //$scope.uploader = new FileUploader();
+    //console.info($scope.uploader)
+
+    /*$scope.uploadFile = function(files) {
+        var fd = new FormData();
+        //Take the first selected file
+        fd.append("file", files[0]);
+
+        $http.post(uploadUrl, fd, {
+            withCredentials: true,
+            headers: {'Content-Type': undefined },
+            transformRequest: angular.identity
+        }).success( ...all right!... ).error( ..damn!... );
+
+    };*/
+
+    $scope.postFile = function () {
+        var formdata = new FormData();
+        formdata.append('xsdScheme', '/home/nikita/Downloads/ex1.xsd');
+        var request = {
+            method: 'POST',
+            url: 'http://localhost:8765/upload?name=check1',
+            data: formdata,
+            headers: {
+                'Content-Type': undefined
+            }
+        };
+        $http(request).then(function (success) {
+              alert("File send");
+        }, function (error) {
+
+        });
     };
-}]);
+
+    $scope.getFile = function () {
+        var request = {
+            method: 'GET',
+            url: 'http://localhost:8765/check1',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+                'Access-Control-Allow-Origin': '*'
+            }
+        };
+
+        $http(request).then(function (success) {
+            alert(data);
+        }, function (error) {
+
+        });
+    };
+});
