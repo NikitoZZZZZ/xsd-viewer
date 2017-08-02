5b14267dae49cfb76995ea8a6bec515611a03f96
NikitoZZZZZ <manakovnikita95@ya.ru>
2017-08-02 17:19:18 +0300

    gradle wrapper support added

--- client/src/main/java/com/emc/xsdviewer/webapp/LandingPageController.java
+++ client/src/main/java/com/emc/xsdviewer/webapp/LandingPageController.java
@@ -4,14 +4,16 @@ package com.emc.xsdviewer.webapp;
  * Created by ivan on 20.07.17.
  */
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Map;
 
 @Controller
 public class LandingPageController {
 
     @RequestMapping("/abc")
-    public String showHomePage(Map<String, Object> model) { return "static/index.html"; }
+    public String showHomePage(Map<String, Object> model) {
+        return "static/index.html";
+    }
 }
