21b32ba63338e39acd306fcd05f3ea824a6e50a0
NikitoZZZZZ <manakovnikita95@ya.ru>
2017-08-16 19:24:55 +0300

    Parser's method "getName" changed

--- server/src/main/java/com/emc/xsdviewer/parser/XsdNodeHandler.java
+++ server/src/main/java/com/emc/xsdviewer/parser/XsdNodeHandler.java
@@ -1,20 +1,69 @@
 package com.emc.xsdviewer.parser;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+
 public class XsdNodeHandler {
-    protected XsdNodeHandler() {
+	private static Map<String, List<String>> types = new HashMap<>();
+	
+    static {
+    	List<String> ls = new ArrayList<>();
+    	ls.add("maxOccurs");
+    	ls.add("minOccurs");
+    	types.put("xs:sequence", ls);
+    	ls = new ArrayList<>();
+    	ls.add("type");
+    	ls.add("nillable");
+    	ls.add("minOccurs");
+    	ls.add("maxOccurs");
+    	types.put("xs:element", ls);
+    	ls = new ArrayList<>();
+    	ls.add("base");
+    	types.put("xs:restriction", ls);
+    	ls = new ArrayList<>();
+    	ls.add("value");
+    	types.put("xs:minInclusive", ls);
+    	ls = new ArrayList<>();
+    	ls.add("value");
+    	types.put("xs:totalDigits", ls);
+    	ls = new ArrayList<>();
+    	ls.add("value");
+    	types.put("xs:minLength", ls);
+    	ls = new ArrayList<>();
+    	ls.add("value");
+    	types.put("xs:maxLength", ls);
     }
 
     public static String createName(Node node) {
+    	String name = "";
         if (node.getAttributes().getNamedItem("name") != null) {
-            return node.getAttributes().getNamedItem("name").toString().substring(5).replaceAll("[\"]", "");
+            name = node.getAttributes().getNamedItem("name").toString().substring(5).replaceAll("[\"]", "");
         }
-        return node.getNodeName();
+        else {
+        	name = node.getNodeName();
+        }
+        for (Map.Entry<String, List<String>> entry : types.entrySet()) {
+        	if(entry.getKey().contains(node.getNodeName())) {
+        		for(int i = 0; i < entry.getValue().size(); ++i) {
+        			try {
+        				name += ", "
+        						+ node.getAttributes().getNamedItem((entry.getValue().get(i))).toString();
+        			}
+        			catch (NullPointerException e) {
+        				
+        			}
+        		}
+        	}
+        }
+        
+        return name;
     }
 
     public static List<XsdNode> createNextNodeList(Node node) {
